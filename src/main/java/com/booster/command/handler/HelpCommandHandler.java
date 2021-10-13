7b7a5bef55caba4589ac1914a514ad4468eaa5da
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-10-13 20:07:46 +0300

    feat: change the 'help' command to print all recognizable commands

--- src/main/java/com/booster/command/handler/HelpCommandHandler.java
+++ src/main/java/com/booster/command/handler/HelpCommandHandler.java
@@ -1,8 +1,11 @@
 package com.booster.command.handler;
 
+import com.booster.command.Command;
 import com.booster.output.CommandLineWriter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
@@ -11,7 +14,14 @@ public class HelpCommandHandler {
     private final CommandLineWriter commandLineWriter;
 
     public void handle() {
-        commandLineWriter.writeLine("Commands are: h e");
+        commandLineWriter.writeLine("Available commands are:");
+        commandLineWriter.newLine();
+
+        Arrays.stream(Command.values())
+                .filter(Command::isRecognizable)
+                .forEach(command -> commandLineWriter.writeLine(command.toString() + ": " + command.getEquivalents()));
+
+        commandLineWriter.newLine();
     }
 
 }
