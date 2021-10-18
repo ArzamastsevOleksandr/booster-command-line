10e74de9fa9bb1748f7f5f736113fce31fe6a549
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-10-18 22:42:06 +0300

    feat: add validation for the ADD_VOCABULARY command

--- src/main/java/com/booster/command/handler/AddVocabularyCommandHandler.java
+++ src/main/java/com/booster/command/handler/AddVocabularyCommandHandler.java
@@ -1,13 +1,11 @@
 package com.booster.command.handler;
 
+import com.booster.command.arguments.AddVocabularyArgs;
 import com.booster.command.arguments.CommandWithArguments;
 import com.booster.dao.VocabularyDao;
-import com.booster.model.Vocabulary;
 import com.booster.output.CommandLineWriter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -17,37 +15,20 @@ public class AddVocabularyCommandHandler {
 
     private final CommandLineWriter commandLineWriter;
 
+    // todo: a common handler interface with a template for actions. Child classes override an abstract action-method.
     // todo: foreign key constraint "vocabulary_entry_vocabulary_id_fkey" on table "vocabulary_entry"
     public void handle(CommandWithArguments commandWithArguments) {
-        List<String> arguments = commandWithArguments.getArguments();
-
-        String name = "";
-        long lblId = -1;
-        for (var arg : arguments) {
-            String[] flagAndValue = arg.split("=");
-            String flag = flagAndValue[0];
-            String value = flagAndValue[1];
-
-            switch (flag) {
-                case "n":
-                    name = value;
-                    break;
-                case "id":
-                    lblId = Long.parseLong(value);
-                    break;
-                default:
-                    // todo: proper error message
-                    System.out.println("ERROR");
-            }
+        if (commandWithArguments.hasNoErrors()) {
+            var args = (AddVocabularyArgs) commandWithArguments.getArgs();
+            vocabularyDao.add(args.getName(), args.getLanguageBeingLearnedId());
+            commandLineWriter.writeLine("Done.");
+        } else {
+            commandLineWriter.writeLine("Errors: ");
+            commandLineWriter.newLine();
+            commandWithArguments.getArgErrors()
+                    .forEach(commandLineWriter::writeLine);
         }
-        var vocabulary = Vocabulary.builder()
-                .name(name)
-                .languageBeingLearnedId(lblId)
-                .build();
-
-        vocabularyDao.add(vocabulary);
-
-        commandLineWriter.writeLine("Done");
+        commandLineWriter.newLine();
     }
 
 }
