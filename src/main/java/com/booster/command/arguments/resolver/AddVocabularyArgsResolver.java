575bf7da5fda8a5eb6ea2988a7184e856c0ab8fb
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-10-18 23:16:47 +0300

    refactor: remove code duplication

--- src/main/java/com/booster/command/arguments/resolver/AddVocabularyArgsResolver.java
+++ src/main/java/com/booster/command/arguments/resolver/AddVocabularyArgsResolver.java
@@ -1,24 +1,21 @@
 package com.booster.command.arguments.resolver;
 
 import com.booster.command.arguments.AddVocabularyArgs;
-import com.booster.command.arguments.Args;
 import com.booster.command.arguments.CommandWithArguments;
 import com.booster.dao.LanguageBeingLearnedDao;
 import com.booster.dao.VocabularyDao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static com.booster.command.Command.ADD_VOCABULARY;
 
 @Component
 @RequiredArgsConstructor
-public class AddVocabularyArgsResolver {
+public class AddVocabularyArgsResolver implements ArgsResolver {
 
     private static final String NAME_FLAG = "n";
     private static final String ID_FLAG = "id";
@@ -48,47 +45,14 @@ public class AddVocabularyArgsResolver {
         }
     }
 
+    @Override
+    public String commandString() {
+        return ADD_VOCABULARY.extendedToString();
+    }
+
     private CommandWithArguments.CommandWithArgumentsBuilder getBuilder() {
         return CommandWithArguments.builder()
                 .command(ADD_VOCABULARY);
-    }
-
-    private void checkIfArgumentsAreSpecified(List<String> args) {
-        if (args.size() == 0) {
-            throw new ArgsValidationException(List.of("No args specified for the " + commandString() + " command."));
-        }
-    }
-
-    private Map<String, String> checkFlagsWithValuesAndReturn(List<String> args) {
-        try {
-            return args.stream()
-                    .map(Args::splitAndStrip)
-                    .collect(Collectors.toMap(arr -> arr[0], arr -> arr[1]));
-        } catch (ArrayIndexOutOfBoundsException e) {
-            List<String> argErrors = List.of(
-                    "Flag must come together with the value, separated by the '=' sign.",
-                    "All flag-value pairs must be separated with a space."
-            );
-            throw new ArgsValidationException(argErrors);
-        }
-    }
-
-    private void checkIfMandatoryFlagsArePresent(Map<String, String> flag2value, Set<String> mandatoryFlags) {
-        var providedFlags = new HashSet<>(flag2value.keySet());
-        var mandatoryFlagsCopy = new HashSet<>(mandatoryFlags);
-        mandatoryFlagsCopy.removeAll(providedFlags);
-        if (!mandatoryFlagsCopy.isEmpty()) {
-            var argErrors = List.of(
-                    "Mandatory flags (" + String.join(",", mandatoryFlagsCopy) + ") are missing"
-            );
-            throw new ArgsValidationException(argErrors);
-        }
-    }
-
-    private void checkIfIdIsCorrectNumber(String idValue) {
-        if (isNotLongType(idValue)) {
-            throw new ArgsValidationException(List.of("Id must be a positive integer number. Got: " + idValue + "."));
-        }
     }
 
     private void checkIfLanguageBeingLearnedExistsWithId(long id) {
@@ -105,19 +69,6 @@ public class AddVocabularyArgsResolver {
                     "Vocabulary with name: " + name + " already exists for the language being learned with id: " + id + "."
             );
             throw new ArgsValidationException(argErrors);
-        }
-    }
-
-    private String commandString() {
-        return ADD_VOCABULARY.extendedToString();
-    }
-
-    private boolean isNotLongType(String s) {
-        try {
-            Long.parseLong(s);
-            return false;
-        } catch (NumberFormatException e) {
-            return true;
         }
     }
 
---
+++ src/main/java/com/booster/command/arguments/resolver/ArgsResolver.java
@@ -0,0 +1,63 @@
+package com.booster.command.arguments.resolver;
+
+import com.booster.command.arguments.Args;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public interface ArgsResolver {
+
+    String commandString();
+
+    default void checkIfArgumentsAreSpecified(List<String> args) {
+        if (args.size() == 0) {
+            List<String> argErrors = List.of("No args specified for the " + commandString() + " command.");
+            throw new ArgsValidationException(argErrors);
+        }
+    }
+
+    default Map<String, String> checkFlagsWithValuesAndReturn(List<String> args) {
+        try {
+            return args.stream()
+                    .map(Args::splitAndStrip)
+                    .collect(Collectors.toMap(arr -> arr[0], arr -> arr[1]));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            List<String> argErrors = List.of(
+                    "Flag must come together with the value, separated by the '=' sign.",
+                    "All flag-value pairs must be separated with a space."
+            );
+            throw new ArgsValidationException(argErrors);
+        }
+    }
+
+    default void checkIfMandatoryFlagsArePresent(Map<String, String> flag2value, Set<String> mandatoryFlags) {
+        var providedFlags = new HashSet<>(flag2value.keySet());
+        var mandatoryFlagsCopy = new HashSet<>(mandatoryFlags);
+        mandatoryFlagsCopy.removeAll(providedFlags);
+        if (!mandatoryFlagsCopy.isEmpty()) {
+            var argErrors = List.of(
+                    "Mandatory flags (" + String.join(",", mandatoryFlagsCopy) + ") are missing"
+            );
+            throw new ArgsValidationException(argErrors);
+        }
+    }
+
+    default void checkIfIdIsCorrectNumber(String idValue) {
+        if (isNotLongType(idValue)) {
+            throw new ArgsValidationException(List.of("Id must be a positive integer number. Got: " + idValue + "."));
+        }
+    }
+
+    private boolean isNotLongType(String s) {
+        try {
+            Long.parseLong(s);
+            return false;
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
+
+}
