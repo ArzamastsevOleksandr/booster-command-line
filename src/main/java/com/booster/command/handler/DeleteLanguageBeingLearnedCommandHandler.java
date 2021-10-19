c1129bfc0238bee6eb40da96c816f20b9239f39b
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-10-19 14:35:23 +0300

    fix: delete all related vocabularies when the corresponding language being learned is deleted

--- src/main/java/com/booster/command/handler/DeleteLanguageBeingLearnedCommandHandler.java
+++ src/main/java/com/booster/command/handler/DeleteLanguageBeingLearnedCommandHandler.java
@@ -3,6 +3,8 @@ package com.booster.command.handler;
 import com.booster.command.arguments.CommandWithArguments;
 import com.booster.command.arguments.DeleteLanguageBeingLearnedArgs;
 import com.booster.dao.LanguageBeingLearnedDao;
+import com.booster.dao.VocabularyDao;
+import com.booster.dao.VocabularyEntryDao;
 import com.booster.output.CommandLineWriter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,13 +14,18 @@ import org.springframework.stereotype.Component;
 public class DeleteLanguageBeingLearnedCommandHandler {
 
     private final LanguageBeingLearnedDao languageBeingLearnedDao;
+    private final VocabularyDao vocabularyDao;
+    private final VocabularyEntryDao vocabularyEntryDao;
 
     private final CommandLineWriter commandLineWriter;
 
-    // todo: if the v exists for this lbl - foreign key constraint "vocabulary_language_being_learned_id_fkey" is fired
     public void handle(CommandWithArguments commandWithArguments) {
         if (commandWithArguments.hasNoErrors()) {
             var args = (DeleteLanguageBeingLearnedArgs) commandWithArguments.getArgs();
+            vocabularyDao.findAllIdsForLanguageBeingLearnedId(args.getId()).forEach(vocabularyId -> {
+                vocabularyEntryDao.deleteAllForVocabularyId(vocabularyId);
+                vocabularyDao.delete(vocabularyId);
+            });
             languageBeingLearnedDao.delete(args.getId());
             commandLineWriter.writeLine("Done.");
         } else {
