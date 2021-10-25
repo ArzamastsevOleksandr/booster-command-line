e62f9391f27ae7d6c58f8c396a69c408f9e017fb
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-10-25 08:23:35 +0300

    test: example unit test; add a list of TODOs

---
+++ src/test/java/com/booster/service/LanguageBeingLearnedServiceTest.java
@@ -0,0 +1,76 @@
+package com.booster.service;
+
+import com.booster.dao.LanguageBeingLearnedDao;
+import com.booster.model.LanguageBeingLearned;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class LanguageBeingLearnedServiceTest {
+
+    static final long ID = 1L;
+
+    LanguageBeingLearnedService languageBeingLearnedService;
+
+    @Mock
+    JdbcTemplate jdbcTemplate;
+
+    LanguageBeingLearnedDao languageBeingLearnedDao;
+    @Spy
+    WrapperService<LanguageBeingLearned> wrapperService;
+
+    @BeforeEach
+    void beforeEach() {
+        languageBeingLearnedDao = Mockito.spy(new LanguageBeingLearnedDao(jdbcTemplate));
+        languageBeingLearnedService = new LanguageBeingLearnedService(languageBeingLearnedDao, wrapperService);
+    }
+
+    @Test
+    void shouldFindLanguageBeingLearnedById() {
+        // given
+        LanguageBeingLearned languageBeingLearned = getLanguageBeingLearned();
+        when(jdbcTemplate.queryForObject(any(String.class), any(RowMapper.class), eq(ID))).thenReturn(languageBeingLearned);
+        // when
+        Optional<LanguageBeingLearned> optionalLanguageBeingLearned = languageBeingLearnedService.findById(ID);
+        // then
+        assertThat(optionalLanguageBeingLearned).contains(languageBeingLearned);
+
+        verify(wrapperService).wrapDataAccessException(any(Supplier.class));
+        verify(languageBeingLearnedDao).findById(ID);
+    }
+
+    @Test
+    void shouldFindLanguageBeingLearnedByLanguageId() {
+        // given
+        LanguageBeingLearned languageBeingLearned = getLanguageBeingLearned();
+        when(jdbcTemplate.queryForObject(any(String.class), any(RowMapper.class), eq(ID))).thenReturn(languageBeingLearned);
+        // when
+        Optional<LanguageBeingLearned> optionalLanguageBeingLearned = languageBeingLearnedService.findByLanguageId(ID);
+        // then
+        assertThat(optionalLanguageBeingLearned).contains(languageBeingLearned);
+
+        verify(wrapperService).wrapDataAccessException(any(Supplier.class));
+        verify(languageBeingLearnedDao).findByLanguageId(ID);
+    }
+
+    private LanguageBeingLearned getLanguageBeingLearned() {
+        return LanguageBeingLearned.builder().build();
+    }
+
+}
