447357f412bba77e2df1839cfb21321afd0a3391
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-10-25 08:23:35 +0300

    test: example unit test; add a list of TODOs

--- src/main/java/com/booster/Main.java
+++ src/main/java/com/booster/Main.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-// todo: standardize maven builds with maven wrapper
-// todo: learn javac
-// todo: java flame graphs
 @Configuration
 @ComponentScan
 public class Main {
@@ -32,6 +29,13 @@ public class Main {
         // todo: later: statistics collector, which commands are used, how often
         // todo: distinguish upper-lower case?
         // todo: pretty print of l, lbl, v, ve, w
+        // todo: I can mark the word as learned for it not to appear in the training sessions
+        // todo: I can mark the word as hard for it to always appear in the training sessions
+        // todo: I can search for the vocabulary entry by a substring
+        // todo: I can move the ve from one v to the other
+        // todo: I can copy the ve from one v to the other
+        // todo: I can create a new ve without specifying the vid - it should appear for the DEFAULT vocabulary
+        // todo: the DEFAULT vocabulary can not be renamed/deleted
         var applicationContext = new AnnotationConfigApplicationContext(Main.class);
         var learningSessionManager = applicationContext.getBean("learningSessionManager", LearningSessionManager.class);
         learningSessionManager.launch();
@@ -39,6 +43,14 @@ public class Main {
         // todo: learn: avoid using null with the help of:
         //  null-safe api (2 methods, 1 accepts a null, 2 has fewer variables and allows you not to pass a null);
         //  custom data structures
+//        ===============================================
+// todo: standardize maven builds with maven wrapper
+// todo: learn javac
+// todo: java flame graphs
+        // todo: inline types: stack gives more cache-friendliness vs heap
+
+// todo: Chapter 33: crash JVM examples
     }
+
 
 }
