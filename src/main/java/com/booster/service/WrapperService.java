1ee083a1b6250548ed1dda6707c87d6da7a64431
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-10-28 18:07:49 +0300

    refactor: code clean up

--- src/main/java/com/booster/service/WrapperService.java
+++ src/main/java/com/booster/service/WrapperService.java
@@ -1,5 +1,7 @@
 package com.booster.service;
 
+import com.booster.output.CommandLineWriter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
@@ -7,13 +9,16 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 @Service
-public class WrapperService<T> {
+@RequiredArgsConstructor
+public class WrapperService {
 
-    public Optional<T> wrapDataAccessException(Supplier<T> s) {
+    private final CommandLineWriter commandLineWriter;
+
+    public <T> Optional<T> wrapDataAccessException(Supplier<T> s) {
         try {
             return Optional.ofNullable(s.get());
         } catch (DataAccessException e) {
-            // todo: handle exceptions
+            commandLineWriter.writeLine("An exception occurred: " + e.getMessage());
             return Optional.empty();
         }
     }
