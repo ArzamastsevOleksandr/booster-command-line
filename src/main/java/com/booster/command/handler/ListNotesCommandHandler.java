f544b0b696769766da2596dcd02f73b640c3c4ae
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-11-04 00:11:28 +0200

    feat: add command to list all notes

---
+++ src/main/java/com/booster/command/handler/ListNotesCommandHandler.java
@@ -0,0 +1,42 @@
+package com.booster.command.handler;
+
+import com.booster.adapter.CommandLineAdapter;
+import com.booster.command.Command;
+import com.booster.command.arguments.CommandWithArgs;
+import com.booster.dao.NoteDao;
+import com.booster.model.Note;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ListNotesCommandHandler implements CommandHandler {
+
+    private final NoteDao noteDao;
+
+    private final CommandLineAdapter adapter;
+
+    @Override
+    public void handle(CommandWithArgs commandWithArgs) {
+        List<Note> notes = noteDao.findAll();
+
+        if (notes.isEmpty()) {
+            adapter.writeLine("There are no notes in the system now.");
+        } else {
+            adapter.writeLine("All notes:");
+            adapter.newLine();
+
+            for (var note : notes) {
+                adapter.writeLine(note.toString());
+            }
+        }
+    }
+
+    @Override
+    public Command getCommand() {
+        return Command.LIST_NOTES;
+    }
+
+}
