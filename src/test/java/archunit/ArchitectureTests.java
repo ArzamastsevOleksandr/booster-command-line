7e65fb0626f8d132c59740febce58ead617ff8ae
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-11-04 14:36:38 +0200

    feat: add plugin to pom.xml to run JUnit tests with maven

--- src/test/java/archunit/ArchitectureTests.java
+++ src/test/java/archunit/ArchitectureTests.java
@@ -1,24 +1,27 @@
 package archunit;
 
+import com.booster.Main;
+import com.booster.launcher.Launcher;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import org.junit.jupiter.api.Test;
 
-public class TestExample {
+class ArchitectureTests {
 
     // fitness functions test how close the current design is to the desired architecture
     @Test
-    void test() {
+    void launcherIsAccessibleOnlyToItselfAndMainClass() {
         JavaClasses javaClasses = new ClassFileImporter().importPackages("com.booster");
 
-        // deliberately fail
         ArchRule rule = ArchRuleDefinition.classes()
                 .that()
-                .resideInAPackage("..handler..")
-                .should().onlyBeAccessed()
-                .byAnyPackage("..input..");
+                .resideInAPackage("..launcher..")
+                .should()
+                .onlyBeAccessed()
+                .byClassesThat()
+                .belongToAnyOf(Main.class, Launcher.class);
 
         rule.check(javaClasses);
     }
