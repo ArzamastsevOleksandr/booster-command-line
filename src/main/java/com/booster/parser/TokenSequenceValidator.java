97138d5d3c212045fd72fccf8de9909fb2750993
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-11-04 14:24:46 +0200

    test: add descriptive error messages for token validation results

--- src/main/java/com/booster/parser/TokenSequenceValidator.java
+++ src/main/java/com/booster/parser/TokenSequenceValidator.java
@@ -10,37 +10,40 @@ public class TokenSequenceValidator {
 
     // todo: package-private access modifiers
     // todo: implement support for flags with no values
-    // todo: add explanatory error messages
     public TokenValidationResult validate(List<Token> tokens) {
         if (tokens.isEmpty()) {
-            return TokenValidationResult.withErrors(List.of("Err0"));
+            return TokenValidationResult.withErrors(List.of("Token sequence must consist of at least one argument"));
         }
         Token expectedCommand = eatFrontCommand(tokens);
         if (TokenType.isNotCommand(expectedCommand.getType())) {
-            return TokenValidationResult.withErrors(List.of("Err1"));
+            return TokenValidationResult.withErrors(List.of("Token sequence must start with a command"));
         }
         if (tokens.size() == 1) {
             return TokenValidationResult.success(tokens);
         }
         List<Token> tokensCopy = new ArrayList<>(tokens.subList(1, tokens.size()));
         if (tokensCopy.size() % 3 != 0) {
-            return TokenValidationResult.withErrors(List.of("Err2"));
+            return TokenValidationResult.withErrors(List.of("Arguments must follow a pattern of flag -> separator -> value"));
         }
         for (int i = 0; i < tokensCopy.size(); i += 3) {
             Token flag = tokensCopy.get(i);
             if (flag.getType() != TokenType.FLAG) {
-                return TokenValidationResult.withErrors(List.of("Err3"));
+                return TokenValidationResult.withErrors(List.of("Expected flag, got: " + flag.getValue() + " with type: " + tokenTypeToLowerCaseString(flag)));
             }
             Token separator = tokensCopy.get(i + 1);
             if (separator.getType() != TokenType.SEPARATOR) {
-                return TokenValidationResult.withErrors(List.of("Err4"));
+                return TokenValidationResult.withErrors(List.of("Expected separator, got: " + separator.getValue() + " with type: " + tokenTypeToLowerCaseString(separator)));
             }
             Token value = tokensCopy.get(i + 2);
             if (value.getType() == TokenType.FLAG) {
-                return TokenValidationResult.withErrors(List.of("Err5"));
+                return TokenValidationResult.withErrors(List.of("Separator is followed by a flag: " + value.getValue()));
             }
         }
         return TokenValidationResult.success(tokens);
+    }
+
+    private String tokenTypeToLowerCaseString(Token flag) {
+        return flag.getType().toString().toLowerCase();
     }
 
     private Token eatFrontCommand(List<Token> tokens) {
