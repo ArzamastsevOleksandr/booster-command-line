ea40c67ebbed04948fa3b466080eb19cf3290b2f
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-11-06 16:26:52 +0200

    refactor: command preprocessor POC

--- src/main/java/com/booster/Main.java
+++ src/main/java/com/booster/Main.java
@@ -9,8 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class Main {
     public static void main(String[] args) {
-        // todo: for commands where pagination is enabled - if none is specified,
-        //  use a preprocessor that will set the default pagination value
+        // todo: use settings in the preprocessor
         // todo: add contexts to the ve
         // todo: s Errors:Token sequence must start with a command
         // todo: I can tag the ve
