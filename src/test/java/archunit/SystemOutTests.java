257e4aa2ae427dbcfb25205284cb0ef045671da5
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-11-29 21:44:30 +0200

    test: ensure that System.out.* calls are only allowed from withing the adapter

---
+++ src/test/java/archunit/SystemOutTests.java
@@ -0,0 +1,36 @@
+package archunit;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static archunit.ArchUnitTestConstants.*;
+
+class SystemOutTests {
+
+    static String ADAPTER_PACKAGE = BOOSTER + PACKAGE_SEPARATOR + ADAPTER;
+
+    JavaClasses javaClasses = new ClassFileImporter().importPackages(BOOSTER);
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.printf("Adapter package: %s\n", ADAPTER_PACKAGE);
+    }
+
+    @Test
+    void outputIsPrintedOnlyByAdapter() {
+        ArchRule rule = ArchRuleDefinition
+                .noClasses()
+                .that()
+                .resideOutsideOfPackage(ADAPTER_PACKAGE)
+                .should()
+                .accessClassesThat()
+                .belongToAnyOf(System.out.getClass());
+
+        rule.check(javaClasses);
+    }
+
+}
