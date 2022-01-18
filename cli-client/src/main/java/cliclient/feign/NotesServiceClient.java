d33ca52581cdedc505fb260a0a8a3eebbf743110
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2022-01-18 02:12:47 +0200

    fix: (cli-client) api update

--- cli-client/src/main/java/cliclient/feign/NotesServiceClient.java
+++ cli-client/src/main/java/cliclient/feign/NotesServiceClient.java
@@ -1,20 +1,22 @@
 package cliclient.feign;
 
-import cliclient.dto.NoteCollection;
+import cliclient.dto.NoteDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @FeignClient(value = "notes-service-client", url = "http://localhost:8081/notes")
 public interface NotesServiceClient {
 
     @GetMapping(value = "/")
-    NoteCollection getAll();
+    Collection<NoteDto> getAll();
 
     @GetMapping(value = "/{id}")
-    NoteResponse findById(@PathVariable("id") Long id);
+    NoteDto findById(@PathVariable("id") Long id);
 
     @PostMapping(value = "/")
-    NoteResponse add(@RequestBody AddNoteInput input);
+    NoteDto add(@RequestBody AddNoteInput input);
 
     @DeleteMapping(value = "/{id}")
     void delete(@PathVariable("id") Long id);
