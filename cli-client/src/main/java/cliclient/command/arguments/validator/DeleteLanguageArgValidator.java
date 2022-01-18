fb2b7402404127bda4ced0dd3f2f49332f131d01
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2022-01-18 17:21:37 +0200

    feat: (cli-client) use a new api to delete a language by id

--- cli-client/src/main/java/cliclient/command/arguments/validator/DeleteLanguageArgValidator.java
+++ cli-client/src/main/java/cliclient/command/arguments/validator/DeleteLanguageArgValidator.java
@@ -2,41 +2,16 @@ package cliclient.command.arguments.validator;
 
 import cliclient.command.Command;
 import cliclient.command.arguments.CommandWithArgs;
-import cliclient.service.LanguageService;
-import cliclient.service.VocabularyEntryService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import static cliclient.command.Command.DELETE_LANGUAGE;
 
 @Component
-@RequiredArgsConstructor
 public class DeleteLanguageArgValidator implements ArgValidator {
-
-    private final LanguageService languageService;
-    private final VocabularyEntryService vocabularyEntryService;
 
     @Override
     public CommandWithArgs validateAndReturn(CommandWithArgs commandWithArgs) {
-        commandWithArgs.getId()
-                .ifPresentOrElse(id -> {
-                    checkIfLanguageExistsWithId(id);
-                    checkIfVocabularyEntriesExistWithLanguageId(id);
-                }, ID_IS_MISSING);
-
         return commandWithArgs;
-    }
-
-    private void checkIfLanguageExistsWithId(Long id) {
-        if (!languageService.existsWithId(id)) {
-            throw new ArgsValidationException("Language does not exist with id: " + id);
-        }
-    }
-
-    private void checkIfVocabularyEntriesExistWithLanguageId(Long id) {
-        if (vocabularyEntryService.existAnyWithLanguageId(id)) {
-            throw new ArgsValidationException("There are vocabulary entries related to this language", "Delete them first");
-        }
     }
 
     @Override
