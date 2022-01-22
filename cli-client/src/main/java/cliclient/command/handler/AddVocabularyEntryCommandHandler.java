7447302fc7debe4ca98f07ae31a53bd8e4755b03
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2022-01-22 15:54:50 +0200

    feat: (cli-client) add vocabulary entry

--- cli-client/src/main/java/cliclient/command/handler/AddVocabularyEntryCommandHandler.java
+++ cli-client/src/main/java/cliclient/command/handler/AddVocabularyEntryCommandHandler.java
@@ -1,71 +1,46 @@
 package cliclient.command.handler;
 
+import api.vocabulary.AddVocabularyEntryInput;
+import api.vocabulary.VocabularyEntryControllerApi;
+import api.vocabulary.VocabularyEntryDto;
 import cliclient.adapter.CommandLineAdapter;
 import cliclient.command.Command;
 import cliclient.command.arguments.AddVocabularyEntryCommandArgs;
 import cliclient.command.arguments.CommandArgs;
-import cliclient.dao.params.AddVocabularyEntryDaoParams;
-import cliclient.model.Settings;
-import cliclient.model.VocabularyEntry;
-import cliclient.model.Word;
-import cliclient.service.*;
-import cliclient.util.StringUtil;
+import cliclient.service.ColorProcessor;
+import cliclient.service.SessionTrackerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.Set;
-
-import static java.util.stream.Collectors.toSet;
 
 @Component
 @RequiredArgsConstructor
 public class AddVocabularyEntryCommandHandler implements CommandHandler {
 
-    private final WordService wordService;
-    private final SettingsService settingsService;
     private final CommandLineAdapter adapter;
-    private final VocabularyEntryService vocabularyEntryService;
     private final SessionTrackerService sessionTrackerService;
     private final ColorProcessor colorProcessor;
-    private final StringUtil stringUtil;
+    private final VocabularyEntryControllerApi vocabularyEntryControllerApi;
 
     @Override
     public void handle(CommandArgs commandArgs) {
         var args = (AddVocabularyEntryCommandArgs) commandArgs;
-        var params = new AddVocabularyEntryDaoParams();
+        var input = new AddVocabularyEntryInput();
 
-        long wordId = wordService.findByNameOrCreateAndGet(args.getName()).getId();
-        params.setWordId(wordId);
+        input.setName(args.getName());
+        args.languageId().ifPresent(input::setLanguageId);
+        args.definition().ifPresent(input::setDefinition);
+        input.setSynonyms(args.getSynonyms());
 
-        args.languageId().ifPresentOrElse(params::setLanguageId, () -> {
-            settingsService.findOne()
-                    .flatMap(Settings::getLanguageId)
-                    .ifPresent(params::setLanguageId);
-        });
-        args.definition().ifPresent(params::setDefinition);
+        VocabularyEntryDto vocabularyEntryDto = vocabularyEntryControllerApi.add(input);
 
-        params.setContexts(args.getContexts());
-        params.setSynonymIds(getWordIds(args.getSynonyms()));
-        params.setAntonymIds(getWordIds(args.getAntonyms()));
-
-        args.tag().ifPresent(tag -> params.setTags(Set.of(tag)));
-
-        VocabularyEntry entry = vocabularyEntryService.addWithDefaultValues(params);
-        adapter.writeLine(colorProcessor.coloredEntry(entry));
+        // todo: color + lastSeen
+        adapter.writeLine(vocabularyEntryDto);
         adapter.writeLine("Entries added so far: " + sessionTrackerService.getVocabularyEntriesAddedCount());
     }
 
     @Override
     public Command getCommand() {
         return Command.ADD_VOCABULARY_ENTRY;
-    }
-
-    private Set<Long> getWordIds(Set<String> words) {
-        return words.stream()
-                .filter(stringUtil::isNotBlank)
-                .map(wordService::findByNameOrCreateAndGet)
-                .map(Word::getId)
-                .collect(toSet());
     }
 
 }
