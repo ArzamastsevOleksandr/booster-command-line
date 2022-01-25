7f3c46bf36a9e8258ffcd96ee146012a2ad385ef
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2022-01-26 00:08:44 +0200

    feat: (tag-service) find tag by id

--- tag-service/src/main/kotlin/tagservice/TagServiceApplication.kt
+++ tag-service/src/main/kotlin/tagservice/TagServiceApplication.kt
@@ -2,8 +2,10 @@ package tagservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["tagservice", "api"])
 class TagServiceApplication
 
 fun main(args: Array<String>) {
