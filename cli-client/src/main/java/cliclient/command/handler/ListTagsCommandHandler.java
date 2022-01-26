dc0716ebe9f33d7b4cfa7a11de4e5872286284b1
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2022-01-26 21:45:31 +0200

    feat: (cli-client) list tags

--- cli-client/src/main/java/cliclient/command/handler/ListTagsCommandHandler.java
+++ cli-client/src/main/java/cliclient/command/handler/ListTagsCommandHandler.java
@@ -1,20 +1,31 @@
 package cliclient.command.handler;
 
+import api.tags.TagDto;
 import cliclient.adapter.CommandLineAdapter;
 import cliclient.command.Command;
 import cliclient.command.arguments.CommandArgs;
+import cliclient.feign.tags.TagServiceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
 
 @Component
 @RequiredArgsConstructor
 public class ListTagsCommandHandler implements CommandHandler {
 
     private final CommandLineAdapter adapter;
+    private final TagServiceClient tagServiceClient;
 
     @Override
     public void handle(CommandArgs commandArgs) {
-        adapter.writeLine("NOT IMPLEMENTED");
+        Collection<TagDto> tags = tagServiceClient.findAll();
+        if (CollectionUtils.isEmpty(tags)) {
+            adapter.writeLine("There are no tags yet");
+        } else {
+            tags.forEach(adapter::writeLine);
+        }
     }
 
     @Override
