9383e12b5f9b710705c299c3e90160be84e5f9e8
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2022-02-14 00:01:30 +0200

    feat: (cli-client) paginate vocabulary entries

--- cli-client/src/main/java/cliclient/postprocessor/CommandWithArgsPostProcessor.java
+++ cli-client/src/main/java/cliclient/postprocessor/CommandWithArgsPostProcessor.java
@@ -2,6 +2,7 @@ package cliclient.postprocessor;
 
 import cliclient.command.Command;
 import cliclient.command.arguments.CommandWithArgs;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -12,8 +13,8 @@ import static java.util.Optional.ofNullable;
 @Component
 public class CommandWithArgsPostProcessor {
 
-    // todo: settings
-    private static final int DEFAULT_PAGINATION = 5;
+    @Value("${fetch.size.vocabulary:5}")
+    private int defaultPagination;
 
     private static final Set<Command> PAGEABLE_COMMANDS = Set.of(
             Command.LIST_VOCABULARY_ENTRIES,
@@ -24,16 +25,16 @@ public class CommandWithArgsPostProcessor {
 
     public CommandWithArgs process(CommandWithArgs commandWithArgs) {
         Command command = commandWithArgs.getCommand();
-        if (commandIsPageable(command)) {
+        if (isPageableCommand(command)) {
             Optional<Integer> pagination = ofNullable(commandWithArgs.getPagination());
             if (pagination.isEmpty()) {
-                commandWithArgs = commandWithArgs.toBuilder().pagination(DEFAULT_PAGINATION).build();
+                commandWithArgs = commandWithArgs.toBuilder().pagination(defaultPagination).build();
             }
         }
         return commandWithArgs;
     }
 
-    private boolean commandIsPageable(Command command) {
+    private boolean isPageableCommand(Command command) {
         return PAGEABLE_COMMANDS.contains(command);
     }
 
