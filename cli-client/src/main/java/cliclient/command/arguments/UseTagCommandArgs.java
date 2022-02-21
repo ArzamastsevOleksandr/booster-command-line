4fc9959a9baf437de6041e4d0ffca1cd168b9218
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2022-02-21 13:46:50 +0200

    feat: (cli-client) display an error message whe USE_TAG is used with no targets

--- cli-client/src/main/java/cliclient/command/arguments/UseTagCommandArgs.java
+++ cli-client/src/main/java/cliclient/command/arguments/UseTagCommandArgs.java
@@ -3,6 +3,5 @@ package cliclient.command.arguments;
 import java.util.Optional;
 
 public record UseTagCommandArgs(String tag,
-                                Optional<Long> noteId,
-                                Optional<Long> vocabularyEntryId) implements CommandArgs {
+                                Optional<Long> noteId) implements CommandArgs {
 }
--- cli-client/src/main/java/cliclient/command/handler/UseTagCommandHandler.java
+++ cli-client/src/main/java/cliclient/command/handler/UseTagCommandHandler.java
@@ -5,6 +5,7 @@ import api.notes.NoteDto;
 import cliclient.adapter.CommandLineAdapter;
 import cliclient.command.Command;
 import cliclient.command.arguments.CommandArgs;
+import cliclient.command.arguments.HelpCommandArgs;
 import cliclient.command.arguments.UseTagCommandArgs;
 import cliclient.feign.notes.NotesServiceClient;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +19,18 @@ public class UseTagCommandHandler implements CommandHandler {
 
     private final CommandLineAdapter adapter;
     private final NotesServiceClient notesServiceClient;
+    private final HelpCommandHandler helpCommandHandler;
 
     @Override
     public void handle(CommandArgs commandArgs) {
         var args = (UseTagCommandArgs) commandArgs;
         args.noteId()
                 .map(id -> addTagsToNote(args, id))
-                .ifPresent(adapter::writeLine);
+                .ifPresentOrElse(adapter::writeLine, () -> {
+                    adapter.error("No target for tag usage specified");
+                    adapter.newLine();
+                    helpCommandHandler.handle(new HelpCommandArgs(Command.USE_TAG));
+                });
     }
 
     private NoteDto addTagsToNote(UseTagCommandArgs args, Long id) {
