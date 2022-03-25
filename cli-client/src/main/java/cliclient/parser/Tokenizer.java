ca4bd1102d5f39234fbe50a0bd4abcd1db8dbdfe
Oleksandr Arzamastsev <oleksandr.arzamastsev@auto1.com>
2022-03-25 20:15:46 +0200

    fix: (cli-client) ADD_VOCABULARY_ENTRY command, allow adding definitions that contain commands

--- cli-client/src/main/java/cliclient/parser/Tokenizer.java
+++ cli-client/src/main/java/cliclient/parser/Tokenizer.java
@@ -133,13 +133,19 @@ class Tokenizer {
         while (i < chars.length && (!FLAG_MARKER.equals(Character.toString(chars[i])))) {
             sb.append(chars[i++]);
             // if the next chunk is command, then we have a [c] [c] sequence
-            if (Character.isWhitespace(chars[i - 1]) && isNextChunkCommand(Arrays.copyOfRange(chars, i, chars.length))) {
+            if (Character.isWhitespace(chars[i - 1]) && isNextChunkCommand(Arrays.copyOfRange(chars, i, chars.length)) && isNotDefinitionPart(tokens)) {
                 addCommandOrText(tokens, sb);
                 return Arrays.copyOfRange(chars, i, chars.length);
             }
         }
         addCommandOrText(tokens, sb);
         return Arrays.copyOfRange(chars, i, chars.length);
+    }
+
+    private boolean isNotDefinitionPart(List<Token> tokens) {
+        return !(tokens.size() > 2
+                && tokens.get(tokens.size() - 1).type() == TokenType.SEPARATOR
+                && tokens.get(tokens.size() - 2).type() == TokenType.FLAG);
     }
 
     private boolean isNextChunkCommand(char[] chars) {
