54b6346fa6fea6430e30c1be79e5cbcce1b92776
Oleksandr Arzamastsev <oleksandr.arzamastsev@auto1.com>
2022-04-02 21:54:52 +0300

    fix: (cli-client) string to command translation

--- notes-service/src/main/java/notesservice/NotesServiceApplication.java
+++ notes-service/src/main/java/notesservice/NotesServiceApplication.java
@@ -1,12 +1,10 @@
 package notesservice;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@Slf4j
 @SpringBootApplication
 @ComponentScan(basePackages = {"notesservice", "api"})
 @EnableFeignClients(basePackages = {"api"})
