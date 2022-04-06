58357daa8c5979feace3e24e1afa386e31741c00
Oleksandr Arzamastsev <oleksandr.arzamastsev@auto1.com>
2022-04-06 19:20:26 +0300

    feat: (mental-arithmetic-service) initial

--- build-and-start-all.sh
+++ build-and-start-all.sh
@@ -6,12 +6,14 @@
 # 8083: upload-service
 # 8084: settings-service
 # 8085: tag-service
+# 8086: mental-arithmetic-service
 sudo kill \
 $(sudo lsof -t -i:8081) \
 $(sudo lsof -t -i:8082) \
 $(sudo lsof -t -i:8083) \
 $(sudo lsof -t -i:8084) \
-$(sudo lsof -t -i:8085)
+$(sudo lsof -t -i:8085) \
+$(sudo lsof -t -i:8086)
 
 # build the whole project and create executable jars while skipping tests
 ./mvnw clean package -Dmaven.test.skip
@@ -22,3 +24,4 @@ java -jar settings-service/target/settings-service-0.0.1-SNAPSHOT.jar &
 java -jar tag-service/target/tag-service-0.0.1-SNAPSHOT.jar &
 java -jar upload-service/target/upload-service-0.0.1-SNAPSHOT.jar &
 java -jar vocabulary-service/target/vocabulary-service-0.0.1-SNAPSHOT.jar
+java -jar mental-arithmetic-service/target/mental-arithmetic-service-0.0.1-SNAPSHOT.jar
---
+++ mental-arithmetic-service/src/main/kotlin/mentalarithmetic/MentalArithmeticServiceApplication.kt
@@ -0,0 +1,11 @@
+package mentalarithmetic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MentalArithmeticServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<MentalArithmeticServiceApplication>(*args)
+}
